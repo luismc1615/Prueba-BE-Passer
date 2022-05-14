1c2878eeeb9a1f5fbeecb227cc157e20fe897686
luismc1615 <58350003+luismc1615@users.noreply.github.com>
2022-05-14 14:49:17 -0600

    Tabla transaction creada

--- passer-be-test/databases/postgresql.js
+++ passer-be-test/databases/postgresql.js
@@ -4,7 +4,13 @@ const postgresql = newDb();
 
 // create mock data
 postgresql.public.none(`create table users(pk_user integer, name text, status boolean);
-                insert into users values (123, 'Juan', true);`);
+                insert into users values (123, 'Juan', true);
+                create table transaction(pk_transaction integer, fk_user integer, description text, amount float);
+                alter table users add constraint pk_user primary key(pk_user);
+                alter table transaction add constraint fk_user foreign key(fk_user) references users(pk_user);
+                insert into transaction values (123, 123, 'Descripción de la transacción', 456);`
+                );
+
 
 
 module.exports = {
